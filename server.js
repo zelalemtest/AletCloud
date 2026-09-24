4bbd346e0d25a53271784d153b4f009035da3fc8
zelalemtest <bbctheone12@gmail.com>
2026-09-24 23:36:15 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const axios = require("axios");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -28,7 +28,26 @@ app.get("/ip", async (req, res) => {
     });
   }
 });
+app.get("/telebirr-test", async (req, res) => {
+  try {
+    const response = await axios.get("https://transactioninfo.ethiotelecom.et/receipt/DHQ46GN88I", {
+      timeout: 15000,
+     
+    });
+	const html = response.data
+    res.status(200).json({data:html});
+  } catch (error) {
+    console.error(error.message);
 
+    if (error.response) {
+      res.status(error.response.status).type("html").send(error.response.data);
+    } else {
+      res.status(500).json({
+        error: error.message
+      });
+    }
+  }
+});
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
